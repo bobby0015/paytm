baae4f61e00fb14937c6224d8ffa576c9b8baaa6
bobby0015 <contact.dcdeveloper@gmail.com>
2026-06-03 23:01:04 +0530

    added new dependencies (jsonwebtoken, body-parser, cors) and crearte the middleware for the same

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express()
 const User = require('./routes/user.route')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 //connection to the database 
 require('./db')
+
+//middlewares
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 
 app.use('/api/v1',User)
 
