0acdf44a1e99e2daa974c9d6b8de0a108f149711
bobby0015 <contact.dcdeveloper@gmail.com>
2026-06-05 00:46:31 +0530

    Created the sigin functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,12 +6,12 @@ const { userInputs } = require('../validation/user.validation')
 const signup = async (req, res) => {
     const { firstName, lastName, email, password } = req.body
 
-    const validInputs = userInputs({firstName,lastName,email,password})
+    const validInputs = userInputs({ firstName, lastName, email, password })
 
-    if(validInputs != true) {
+    if (validInputs != true) {
         return res.status(400).json({
-            msg : validInputs,
-            success : false
+            msg: validInputs,
+            success: false
         })
     }
 
@@ -44,7 +44,7 @@ const signup = async (req, res) => {
                     return res.status(200).json({
                         msg: 'User account created successfully !',
                         success: true,
-                        user: { data : newUser, token: newUserToken }
+                        user: { data: newUser, token: newUserToken }
                     })
 
                 } catch (err) {
@@ -66,8 +66,50 @@ const signup = async (req, res) => {
 
 }
 
-const signin = (req, res) => {
+const signin = async (req, res) => {
+    const { email, password } = req.body
 
+    try {
+        const user = await userModel.findOne({email})
+        
+        if (!user) {
+            return res.status(404).json({
+                msg: 'User not found',
+                success: false
+            })
+        }
+
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (!result) {
+                return res.status(409).json({
+                    msg: "Enter the correct password",
+                    success: false
+                })
+            }
+
+            try {
+                const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
+
+                return res.status(200).json({
+                    msg : 'User logged in successfully',
+                    success : true,
+                    token
+                })
+
+            } catch (err) {
+                res.status(500).json({
+                    msg: 'Something went wrong',
+                    success: false
+                })
+            }
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            msg: 'Something went wrong',
+            success: false
+        })
+    }
 }
 
 module.exports = {
