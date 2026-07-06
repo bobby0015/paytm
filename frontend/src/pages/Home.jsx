bcd4850189b18697df703d037df6b352f03cbeea
bobby0015 <contact.dcdeveloper@gmail.com>
2026-07-07 04:36:13 +0530

    Designed the home page from where we can serch the user and send the money

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,15 @@
-
+import { Input } from "@/components/ui/input"
+import UserList from "@/components/UserList"
 
 const Home = () => {
   return (
-    <div className="flex justify-center">
+    <div className="w-[90%] mx-auto mt-16">
+      <h1 className="text-xl font-semibold">Your Balance : ₹ 5000</h1>
+      <div className="my-4">
+        <h2 className="font-semibold">Users</h2>
+        <Input placeholder="Search Users" />
+      </div>
+      <UserList />
     </div>
   )
 }
